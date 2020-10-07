52db776b4776d9e76d7690765d4a314267b024b8
Chris Spohn <chris.j.spohn@gmail.com>
2020-10-07 16:25:51 -0600

    complete first three function tests in meal-test.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,28 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return  `Delicious ${name}`;
+}
+
+function createMenuItem(item, price, meal) {
+  return {
+    name: item,
+    price: price,
+    type: meal
+  };
+}
+
+function addIngredients(ingredient, ingredientArray) {
+  //if we cannot find the index of the ingredient in our array it must not exist, so add it.
+  if (ingredientArray.indexOf(ingredient) === -1) {
+    ingredientArray.push(ingredient)
+  }
 
 }
 
-
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
