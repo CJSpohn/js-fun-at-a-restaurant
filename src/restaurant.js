574665a63029dfbe6d2eae6ae44190b0d3bef3be
Chris Spohn <chris.j.spohn@gmail.com>
2020-10-08 20:26:09 -0600

    refactor restaurant.js with .includes() function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,6 @@
 function createRestaurant(name) {
   return {
-    //name property equal to given name
+    //name property value equal to name given when called
     name: name,
     //menus only need to be empty arrays
     menus: {
@@ -12,23 +12,9 @@ function createRestaurant(name) {
 }
 
 function addMenuItem(restaurant, item) {
-  //This block will see if our item already exists and break out before adding a duplicate item.
-  //Use a for..in loop to iterate through our objects.
-  for (var menu in restaurant.menus) { //iterate through each menu
-    for (var dish in restaurant.menus[menu]) {//iterate through each food item in that menu
-      if (restaurant.menus[menu][dish] === item) { //If any dish is deep equal to the item we're passing in then return
-        return; //If we get here we found a dupilcate, so breakout before adding below.
-      }
-    }
-  }
-  //If the menu item doesn't already exist, add it to the correct menu based on
-  //the type property of the item being passed in.
-  if (item.type === "breakfast") { //a simlpe if/else statement can tackle this
-    return restaurant.menus.breakfast.push(item); //return the item being pushed to correct menu
-  } else if (item.type === "lunch") {
-    return restaurant.menus.lunch.push(item);
-  } else if (item.type === "dinner") {
-    return restaurant.menus.dinner.push(item);
+  //if the item is NOT included then we push it to the correct array
+  if (!restaurant.menus[item.type].includes(item)) {
+    return restaurant.menus[item.type].push(item);
   }
 }
 
@@ -37,16 +23,15 @@ function removeMenuItem(restaurant, name, menu) {
   var itemDeletedString = `No one is eating our ${name} - it has been removed from the ${menu} menu!`
   var notOnMenuString = `Sorry, we don't sell ${name}, try adding a new recipe!`
   //iterate through each menu/item with a nested for..in statement
-  for (var menu in restaurant.menus) { //for each menu
-    for (var dish in restaurant.menus[menu]) { //for each dish on each menu
-      if (restaurant.menus[menu][dish].name === name) { //if the names are the same (meaning duplicate):
-        //remove the item from the array with .splice(), the index of that item is equal to
-        //our dish variable (the nth item iterated through in the second for/in loop). We remove one element.
-        restaurant.menus[menu].splice(dish, 1);
-        return itemDeletedString; //return our string letting the user know it was deleted.
-      }
+  for (var dish in restaurant.menus[menu]) { //for each dish on each menu
+    if (restaurant.menus[menu][dish].name === name) { //if the names are the same (meaning duplicate):
+      //remove the item from the array with .splice(), the index of that item is equal to
+      //our dish variable (the nth item iterated through in the second for/in loop). We remove one element.
+      restaurant.menus[menu].splice(dish, 1);
+      return itemDeletedString; //return our string letting the user know it was deleted.
     }
   }
+
   //if we make it here we didn't find an item with the given name on any of our menus
   return notOnMenuString
 }
