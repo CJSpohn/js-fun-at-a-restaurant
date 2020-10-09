b1a9fcaae98c212df38cfabc168c2e637be47a49
Chris Spohn <chris.j.spohn@gmail.com>
2020-10-08 20:57:32 -0600

    Refactor: Chef.js with .includes()

--- src/Chef.js
+++ src/Chef.js
@@ -5,15 +5,15 @@ class Chef {
   }
 
   greetCustomer(custName, isMorning) {
-    //return (the boolean of isMorning. true means return first statement : false means return second statement)
+    //return (the boolean of isMorning. if true return this statement : if false return this statement)
     return isMorning ? `Good morning, ${custName}!` : `Hello, ${custName}!`
   }
 
 /*
 
-I know ternary operators are seemingly frowned upon by every instructor but I like
+I know ternary operators were frowned upon by every instructor but I like
 that they only use one line when they are very simple like the one above. I included
-the if/else statement that my ternary operator is replacing just to make it clear I 
+the if/else statement that my ternary operator is replacing just to make it clear I
 am comfortable using either operation. I'm hoping it's okay since I only did it in this extension.
 
 if (isMorning) {
@@ -24,18 +24,15 @@ if (isMorning) {
 */
 
   checkForFood(foodItem) {
-    var serving = `Yes, we're serving ${foodItem.name} today!`
-    var sorryMsg = `Sorry, we aren't serving ${foodItem.name} today.`
-    //using the same for in loop logic from addMenuItem in restaurant.js
-    for (var menu in this.restaurant.menus) { //iterate through menus
-      for (var dish in this.restaurant.menus[menu]) { //iterate through dishes
-        if (this.restaurant.menus[menu][dish] === foodItem) { //if we find the requested dish
-          return serving //let people know we are serving it
-        }
-      }
+    //Here I prefer to use an if/else instead of a ternary because it's so long.
+    //if the menu corresponding to the foodItem menu value contains our food item:
+    if (this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      //let people know we are serving it
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+      //if we cannot find it, return our sorry message
+      return `Sorry, we aren't serving ${foodItem.name} today.`
     }
-    //if we cannot find it, return our sorry message
-    return sorryMsg
   }
 }
 
